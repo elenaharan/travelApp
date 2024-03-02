92f351aacb99e54dcb094f77ac859711888bcec7
Elena <elenaharan@gmail.com>
2024-03-02 08:43:01 +0000

    Add stats with derived state

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onHandleCheckItem={handleCheckItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -110,12 +110,25 @@ function PackingListItem({ item, onDeleteItem, onHandleCheckItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed === true).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
-    <em>
-      <footer className="stats">
-        🧳 You have x items on your list, and you already packed X (%)
-      </footer>
-    </em>
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You've got everything! Ready to go ✈️"
+          : `🧳 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
   );
 }
